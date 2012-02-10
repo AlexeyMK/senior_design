d2757fdafef3235d6d8dec1d75f07f0d43fc5472
Alexey Komissarouk <AlexeyMK@gmail.com>
2012-02-09 21:18:31 -0500

    Initial version of setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+from setuptools import setup
+
+setup(
+  name="marketplacr experiment",
+  author="Alexey Komissarouk",
+  author_email="alexeymk@gmail.com",
+  url="http://github.com/alexeymk/senior_design",
+  install_requires = ['boto', 'CherryPy', 'google-appengine'],
+  )
+
 #TODO - set up a proper setup.py
 #requirements so far: cherrypy (pip install cherrypy)
+#boto
+#google app engine
 
